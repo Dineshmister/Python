d6f84d6aaee9e1df0a1b6ce931af5ae4ff39d453
Dineshmister <90615630+Dineshmister@users.noreply.github.com>
2021-09-17 16:52:26 +0530

    Add files via upload

---
+++ Factorial using recursive.py
@@ -0,0 +1,7 @@
+#Factorial using reduce
+n = int(input("Enter the no to find factorial :" ))
+#lis is created using list comprehension
+lis = [x for x in range(1,n+1)]
+#reduce is used to multiply a,b one by one from the lis
+c = reduce(lambda a,b:a*b,lis)
+print("Factorial of ",+n,"is : ",c)
