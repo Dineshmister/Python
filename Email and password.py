2a56fc776fab5011dec860cc5ecfa99d3bbfd73b
Dineshmister <90615630+Dineshmister@users.noreply.github.com>
2021-09-17 17:01:21 +0530

    Update Email and password.py

--- Email and password.py
+++ Email and password.py
@@ -17,31 +17,31 @@ password()
 class passwordchecking:
     
 	def length_check(self):
-     #user_input variable is created for getting the password from the user
-     user_input = str(input("Create your password : "))
-     
-	 #this pattern checks for the length whether it lies between 5-20
-     length = re.compile("[A-Z0-9a-z]{5,20}")
-     
-	 #search is used to check the pattern is present
-     operation = length.search(user_input)
-     
-	 #period is another condition whether it checks whether the entered password contains any periods
-     period = re.search("\.",user_input)
-     
-	 #if it contains it give the message from print statement and recursively call the function until it get corrected
-     if period:
-         print("Your password shoudn't contain periods")
-         self.length_check()
-     
-	 #after it checks for the periods it will check for the length that assigned in operation variable
-     elif operation:
-         print("Password is correct")
-     else:
-        #if the entered password contains less than 5 digits it will print the statement and the length_check function will be called again
-        print("your password should contain atleast 5 digits")
-        self.length_check()
-
+		#user_input variable is created for getting the password from the user
+		user_input = str(input("Create your password : "))
+		
+		#this pattern checks for the length whether it lies between 5-20
+		length = re.compile("[A-Z0-9a-z]{5,20}")
+		
+		#search is used to check the pattern is present
+		operation = length.search(user_input)
+		
+		#period is another condition whether it checks whether the entered password contains any periods
+		period = re.search("\.",user_input)
+		
+		#if it contains it give the message from print statement and recursively call the function until it get corrected
+		if period:
+			print("Your password shoudn't contain periods")
+			self.length_check()
+			
+		#after it checks for the periods it will check for the length that assigned in operation variable
+		elif operation:
+		   print("Password is correct")
+		
+		else:
+		   #if the entered password contains less than 5 digits it will print the statement and the length_check function will be called again
+		   print("your password should contain atleast 5 digits")
+		   self.length_check()
 #object is created for class
 call = passwordchecking()
 #method is called through object 'call'
